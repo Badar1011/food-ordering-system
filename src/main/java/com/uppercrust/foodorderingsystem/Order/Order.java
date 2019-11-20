40c9515f2ebd8218daa742a457b227d84e22c498
Badar Munir <b.munir128@gmail.com>
2019-11-20 21:19:06 +0000

    fixed the issue with the order table

--- src/main/java/com/uppercrust/foodorderingsystem/Order/Order.java
+++ src/main/java/com/uppercrust/foodorderingsystem/Order/Order.java
@@ -1,4 +1,22 @@
 package com.uppercrust.foodorderingsystem.Order;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity(name = "order_table")
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
 }
